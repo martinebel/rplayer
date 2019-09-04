dbd8429e3d924fe2a9bfb7135c9aff1189fcdd5b
martinebel <36831742+martinebel@users.noreply.github.com>
2019-09-04 19:42:02 -0300

    UI

--- assets/js/actions/player.js
+++ assets/js/actions/player.js
@@ -1,111 +1,144 @@
 var estado=0;
 
 function initPlayer(){
-//get data
-$.ajax({
-url: 'api/player.php?action=getData&idCliente='+localStorage.getItem("idCliente"),
-async: true,
-contentType: "application/json",
-   success: function(data) {
-	   var obj=JSON.parse(data);
-
-    for(var i=0;i<obj.length;i++)
-    {
-      $("#nombreGrupo").html(obj[i].nombre);
-      document.title=obj[i].nombre;
-      $("#estado").html("Esperando...");
-      $("#archivo").html('<source src="'+obj[i].archivo+'" type="audio/mpeg">');
-      if(obj[i].logo!=""){
-      $("body").css("background-image","url('"+obj[i].logo+"')");
-      $("body").css("background-size","contain");
-      $("#player").fadeIn();
-    }
-      window.setInterval(function(){
-      startMonitoring();
-    }, 1000);
-    }
-
-   }
-});
-}
-
-
-function startMonitoring()
-{
-  if(estado==0)
-  {
-    $.ajax({
-    url: 'api/player.php?action=getStatus&idCliente='+localStorage.getItem("idCliente"),
+  //obtener los datos desde el servidor
+  $.ajax({
+    url: 'api/player.php?action=getData&idCliente='+localStorage.getItem("idCliente"),
     async: true,
     contentType: "application/json",
-       success: function(data) {
-    	   var obj=JSON.parse(data);
-         if(obj=="1")
-         {
-           estado=1;
-           //start playing
-           var media = document.getElementById("archivo");
-           const playPromise = media.play();
-           if (playPromise !== null){
-               playPromise.catch(() => { media.play(); })
-           }
-           $("#estado").html("Reproduciendo");
-         }
+    success: function(data) {
+      var obj=JSON.parse(data);
 
-       }
+      for(var i=0;i<obj.length;i++)
+      {
+        $("#nombreGrupo").html(obj[i].nombre); //nombre del grupo
+        document.title=obj[i].nombre;
+        $("#estado").html("Esperando..."); //estado inicial
+        $("#archivo").html('<source src="'+obj[i].archivo+'" type="audio/mpeg">'); //cargar el audio
+        if(obj[i].logo!=""){ //si hay una imagen, ponerla de background
+          $("body").css("background-image","url('"+obj[i].logo+"')");
+          $("body").css("background-size","contain");
+          $("#player").fadeIn(); //mostrar el panel del reproductor
+        }
+        //iniciar cada 5seg la funcion que revisa el estado enviado por el servidor
+        window.setInterval(function(){
+          startMonitoring();
+        }, 1000);
+      }
+
+    }
+  });
+}
+
+//revisa si el servidor nos manda un estado
+function startMonitoring()
+{
+  if(estado==0) //estoy en stop
+  {
+    $.ajax({
+      url: 'api/player.php?action=getStatus&idCliente='+localStorage.getItem("idCliente"),
+      async: true,
+      contentType: "application/json",
+      success: function(data) {
+        var obj=JSON.parse(data);
+        if(obj=="1") //si nos manda play
+        {
+          estado=1;
+          //start playing
+
+          var media = document.getElementById("archivo");
+          const playPromise = media.play();
+
+          if (playPromise !== null){
+            playPromise.catch(() => { media.play(); })
+          }
+
+          $("#notworking").show(); //mostrar el boton de "no funciona?"
+
+          $("#estado").html("Reproduciendo"); //cambiar el subtitulo de estado
+        }
+
+      }
     });
   }
 }
 
+//click en el boton Repetir
 $(document).on('click','#repetir',function(e){
 
-  //change status on server
+  //aviso al servidor que estoy en play
   $.ajax({
-  url: 'api/player.php?action=setStatus&idCliente='+localStorage.getItem("idCliente")+'&status=1',
-  async: true,
-  contentType: "application/json",
-     success: function(data) {
-       estado=1;
-       //start playing
-       var media = document.getElementById("archivo");
-       const playPromise = media.play();
-       if (playPromise !== null){
-           playPromise.catch(() => { media.play(); })
-       }
-       $("#estado").html("Reproduciendo");
-       $("#repeat").hide();
-     }
+    url: 'api/player.php?action=setStatus&idCliente='+localStorage.getItem("idCliente")+'&status=1',
+    async: true,
+    contentType: "application/json",
+    success: function(data) {
+      estado=1; //estoy en play
+      //start playing
+      var media = document.getElementById("archivo");
+      const playPromise = media.play();
+      if (playPromise !== null){
+        playPromise.catch(() => { media.play(); })
+      }
+      $("#estado").html("Reproduciendo"); //cambiar el subtitulo de estado
+      $("#repeat").hide(); //ocultar este boton
+    }
   });
 });
 
+
+//click en el boton "Recargar"
 $(document).on('click','#recargar',function(e){
 
-  //change status on server
+  //aviso al servidor que estoy en stop
   $.ajax({
-  url: 'api/player.php?action=setStatus&idCliente='+localStorage.getItem("idCliente")+'&status=0',
-  async: true,
-  contentType: "application/json",
-     success: function(data) {
-       location.reload();
-     }
+    url: 'api/player.php?action=setStatus&idCliente='+localStorage.getItem("idCliente")+'&status=0',
+    async: true,
+    contentType: "application/json",
+    success: function(data) {
+      location.reload(); //recargo la pagina
+    }
   });
 });
 
+
 var aud = document.getElementById("archivo");
-aud.onended = function() {
-  //change status on server
+aud.onended = function() { //evento del audio: cuando finaliza el archivo que se esta reproduciendo
+  //aviso al servidor que estoy en stop
   $.ajax({
-  url: 'api/player.php?action=setStatus&idCliente='+localStorage.getItem("idCliente")+'&status=2',
-  async: true,
-  contentType: "application/json",
-     success: function(data) {
-       estado=0;
-     }
+    url: 'api/player.php?action=setStatus&idCliente='+localStorage.getItem("idCliente")+'&status=2',
+    async: true,
+    contentType: "application/json",
+    success: function(data) {
+      estado=0; //estoy en stop
+    }
   });
-  $("#estado").html("Finalizado");
-  $("#repeat").show();
+  $("#estado").html("Finalizado");  //cambiar el subtitulo de estado
+  $("#repeat").show(); //mostrar el boton Repetir
 };
-aud.ontimeupdate = function() {
+aud.ontimeupdate = function() { //evento del audio: cuando avanza la reproduccion
   var progreso=aud.currentTime / aud.duration * 100 ;
-  $('.progress-bar').css('width', progreso+'%').attr('aria-valuenow', progreso);
+  $('.progress-bar').css('width', progreso+'%').attr('aria-valuenow', progreso); //muestro el progreso del audio
 };
+
+//click en el boton "no funciona?"
+$(document).on('click','#notworking',function(e){
+  //pido los datos al servidor
+  $.ajax({
+    url: 'api/player.php?action=getData&idCliente='+localStorage.getItem("idCliente"),
+    async: true,
+    contentType: "application/json",
+    success: function(data) {
+      var obj=JSON.parse(data);
+
+      for(var i=0;i<obj.length;i++)
+      {
+        //recargo el audio
+        $("#archivo").html('<source src="'+obj[i].archivo+'" type="audio/mpeg">');
+        $("#archivo").attr("controls","controls"); //muestro los controles html5 nativos
+        $("#barra").hide(); //oculto barra de progreso
+      }
+
+    }
+  });
+
+});
--- assets/js/actions/register.js
+++ assets/js/actions/register.js
@@ -1,4 +1,4 @@
-//get groups list
+//obtengo la lista de grupos
 $.ajax({
 url: 'api/register.php?action=getGroups',
 async: true,
@@ -8,23 +8,27 @@ contentType: "application/json",
 
     for(var i=0;i<obj.length;i++)
     {
+      //cargo en el combo
       $("#grupo").append('<option value="'+obj[i].idgrupo+'">'+obj[i].nombre+'</option>');
     }
 
+//si ya existe una key, quiere decir que el cliente ya esta registrado
+//y volvio a entrar (seguro porque recargó la pagina)
+//entonces tengo que autocompletar los datos y deshabilitarlos (para que no cambien)
     if (localStorage.getItem("keyCliente") === null) {
 
   }
   else {
-    $("#nombre").val(localStorage.getItem("nombreCliente"));
-    $("#grupo").val(localStorage.getItem("grupoCliente"));
-    $("#nombre").hide();
-    $("#grupo").hide();
+    $("#nombre").val(localStorage.getItem("nombreCliente")); //cargo el nombre
+    $("#grupo").val(localStorage.getItem("grupoCliente")); //cargo el grupo que tenia
+    $("#nombre").hide(); //oculto el campo de nombre
+    $("#grupo").hide(); //oculto el campo de seleccion de grupo
   }
 
    }
 });
 
-//get app name and logo
+//obtengo el nombre de la app y la imagen de fondo
 $.ajax({
 url: 'api/register.php?action=getConfig',
 async: true,
@@ -34,9 +38,9 @@ contentType: "application/json",
 
     for(var i=0;i<obj.length;i++)
     {
-      $("#titulo").html(obj[i].appname);
-      document.title=obj[i].appname;
-      if(obj[i].logo!=""){
+      $("#titulo").html(obj[i].appname); //muestro el titulo
+      document.title=obj[i].appname; //muestro el titulo en la barra del navegador o en la pestaña
+      if(obj[i].logo!=""){ //si hay imagen la pongo en background
       $("body").css("background-image","url('"+obj[i].logo+"')");
       $("body").css("background-size","contain");
     }
@@ -46,26 +50,27 @@ contentType: "application/json",
 });
 
 
-//register button
+//boton "Entrar"
 $(document).on('click','#register',function(e){
-  //save local variables
+  //guardo las variables
   if (localStorage.getItem("keyCliente") === null) {
   localStorage.setItem("nombreCliente",$("#nombre").val());
   localStorage.setItem("grupoCliente",$("#grupo").val());
-  //generate local key
+  //genero una key para este cliente
   localStorage.setItem("keyCliente",jQuery.now());
 }
-  //send data to server
+  //mando los datos al servidor
   $.ajax({
   url: 'api/register.php?action=register&nombre='+localStorage.getItem("nombreCliente")+"&grupo="+localStorage.getItem("grupoCliente")+"&key="+localStorage.getItem("keyCliente"),
   async: true,
   contentType: "application/json",
      success: function(data) {
-       //get client id and redirect to player
+       //el servidor me devuelve una ID para este cliente. la guardo
         var obj=JSON.parse(data);
        localStorage.setItem("idCliente",obj);
-       //window.location.href="player.html";
+       //oculto el panel de inicio
        $("#login").hide();
+       //inicializo el reproductor
        initPlayer();
 
      }
